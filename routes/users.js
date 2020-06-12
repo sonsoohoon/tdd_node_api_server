15d98e7d35eccdfaae3a513f77173380cca80f7b
shson <jebi10mari@naver.com>
2020-06-12 11:38:19 +0900

    first commit

---
+++ routes/users.js
@@ -0,0 +1,17 @@
+const express = require('express');
+const router = express.Router();
+const controller = require('../controller/user.controller');
+
+/* GET users listing. */
+router.get('/', controller.index);
+
+router.get('/:id', controller.show)
+
+router.delete('/:id', controller.destroy)
+
+router.post('/', controller.create)
+
+router.put('/:id', controller.update)
+
+module.exports = router;
+
